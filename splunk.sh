9631b2daa50b1f65c41a5d9d9efeffd05e5db7f2
Diksha Bajaj <47319191+Diksha86@users.noreply.github.com>
2019-10-22 12:17:25 +0000

    Update splunk.sh

--- splunk.sh
+++ splunk.sh
@@ -1,6 +1,6 @@
 sudo apt-get install wget -y
 sudo wget -O splunk-7.1.0-2e75b3406c5b-linux-2.6-amd64.deb 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=7.1.0&product=splunk&filename=splunk-7.1.0-2e75b3406c5b-linux-2.6-amd64.deb&wget=true'
 sudo dpkg -i splunk-7.1.0-2e75b3406c5b-linux-2.6-amd64.deb
-sudo cd /opt/splunk/bin
+cd /opt/splunk/bin
 sudo ./splunk enable boot-start --accept-license
 sudo ./splunk start
