e46ae37e8c4ff53573bdf797576a7d8c2f4fdff5
Diksha Bajaj <47319191+Diksha86@users.noreply.github.com>
2019-10-22 06:54:16 +0000

    Create sonarqube.tf

---
+++ sonarqube.tf
@@ -0,0 +1,44 @@
+provider "google" {
+  credentials = "${file("${var.credentials}")}"
+  project     = "${var.gcp_project}"
+  region      = "${var.region}"
+}
+
+//SonarQube Instance
+resource "google_compute_address" "sonarqubeip" {
+  name   = "${var.sonarqube_instance_ip_name}"
+  region = "${var.sonarqube_instance_ip_region}"
+}
+
+
+resource "google_compute_instance" "sonarqube" {
+  name         = "${var.instance_name}"
+  machine_type = "n1-standard-1"
+  zone         = "us-east1-b"
+
+  tags = ["name", "splunk", "http-server"]
+
+  boot_disk {
+    initialize_params {
+      image = "centos-7-v20180129"
+    }
+  }
+
+  // Local SSD disk
+  #scratch_disk {
+  #}
+
+  network_interface {
+    network    = "${var.sonarvpc}"
+    subnetwork = "${var.sonarsub}"
+    access_config {
+      // Ephemeral IP
+      nat_ip = "${google_compute_address.sonarqubeip.address}"
+    }
+  }
+  metadata = {
+    name = "splunk"
+  }
+
+  metadata_startup_script = "sudo yum update -y;sudo yum install git -y; sudo git clone https://github.com/Diksha86/splunk.git; cd /splunk; sudo chmod 777 /splunk/*; sudo sh splunk.sh"
+}
