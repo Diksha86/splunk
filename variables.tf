11b7950d24c3120898e6735405f97e48d1bdc2ba
Diksha Bajaj <47319191+Diksha86@users.noreply.github.com>
2019-10-23 09:51:58 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
 variable "region" {}
 variable "gcp_project" {}
 variable "credentials" {}
-variable "sonarvpc" {}
-variable "sonarsub" {}
+variable "splunkvpc" {}
+variable "splunksub" {}
 // variable "elk_instance_name" {}
 variable "instance_name" {}
-variable "sonarqube_instance_ip_name" {}
-variable "sonarqube_instance_ip_region" {}
+variable "splunk_instance_ip_name" {}
+variable "splunk_instance_ip_region" {}
